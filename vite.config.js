a6e9055c3d927f796dfae17d3e9922727869ec80
Camilo Portilla <31325055+harem123@users.noreply.github.com>
2023-02-14 07:14:56 -0500

    Update vite.config.js
    add port config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+
+export default ({ mode }) => {
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+            port: parseInt(process.env.VITE_PORT),
+        },
 })
